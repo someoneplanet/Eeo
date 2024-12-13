3112822c781e94be3ebb23a8269c88a1eed08f91
someoneplanet <moaipp9@gmail.com>
2024-12-12 21:23:05 -0300

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update and upgrade Termux packages
-echo "Updating and upgrading Termux..."
+echo "Updating and upgrading Termux packages..."
 pkg update && pkg upgrade -y
 
 # Install necessary Termux packages
@@ -13,13 +13,13 @@ echo "Installing Debian distribution..."
 proot-distro install debian
 
 # Log into Debian and install necessary software
-echo "Setting up Debian with all required packages..."
+echo "Setting up Debian with required packages..."
 proot-distro login debian <<EOF
     echo "Updating and upgrading Debian..."
     apt update && apt upgrade -y
 
     echo "Installing software and tools..."
-    apt install -y curl wget git nano vim unzip zip htop screen tmux neofetch lynx w3m firefox-esr chromium xorg x11-utils x11-apps xfce4 xfce4-goodies openssh-client openssh-server net-tools nmap traceroute build-essential gcc g++ make python3 python3-pip ffmpeg vlc gimp rsync rar unrar p7zip-full tree synaptic apt-file ncdu gparted glances tigervnc-server xrdp libreoffice evince
+    apt install -y curl wget git nano vim unzip zip htop screen tmux neofetch lynx w3m firefox-esr chromium xorg x11-utils x11-apps xfce4 xfce4-goodies openssh-client openssh-server net-tools nmap traceroute build-essential gcc g++ make python3 python3-pip ffmpeg vlc gimp rsync rar unrar p7zip-full tree synaptic apt-file ncdu gparted glances tigervnc-standalone-server tigervnc-viewer xrdp libreoffice evince
 
     echo "Cleaning up unnecessary packages..."
     apt autoremove -y && apt autoclean -y
@@ -27,17 +27,9 @@ proot-distro login debian <<EOF
     echo "Updating apt-file cache..."
     apt-file update
 
-    echo "Configuring VNC server..."
-    mkdir -p ~/.vnc
-    echo '#!/bin/bash' > ~/.vnc/xstartup
-    echo 'startxfce4 &' >> ~/.vnc/xstartup
-    chmod +x ~/.vnc/xstartup
-    tightvncserver :1
-
-    echo "Debian setup complete! You can log in and start the VNC server using:"
-    echo "proot-distro login debian"
-    echo "tightvncserver :1"
+    echo "Debian setup complete! To start the VNC server, use:"
+    echo "vncserver :1"
 EOF
 
 # Completion message
-echo "Setup complete! Use a VNC client to connect to 'use a vnc like realvnc or something to connect to localhost:1 I think it's that?'."
+echo "Setup complete! Log into Debian with 'proot-distro login debian' and start the VNC server with 'vncserver :1'. Connect to the server using 'localhost:1'."
